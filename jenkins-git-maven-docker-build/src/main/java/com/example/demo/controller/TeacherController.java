b8ee809b3cb4d66cf1338f606c51c1acadab4068
Sumitha.M <Sumitha.M@EXIND3010-51.allfon.in>
2022-03-21 17:03:22 +0530

    request

--- jenkins-git-maven-docker-build/src/main/java/com/example/demo/controller/TeacherController.java
+++ jenkins-git-maven-docker-build/src/main/java/com/example/demo/controller/TeacherController.java
@@ -26,5 +26,12 @@ public class TeacherController {
 	}
 	return list.toString();
 	}
+	@GetMapping(path="/teachers/{id}")
+	public String getTeacherById(@PathVariable("id") int id) {
+	List<String> list = Arrays.asList("amar","reka","sumi");
+	
+	
+	return"best techer";
+	}
 }
 
