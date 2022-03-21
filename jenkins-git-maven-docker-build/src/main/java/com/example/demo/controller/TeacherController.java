9ce7fd4765156479b927ab53a4b1d3d4c334ded0
Sumitha.M <Sumitha.M@EXIND3010-51.allfon.in>
2022-03-21 12:49:55 +0530

    request 102

--- jenkins-git-maven-docker-build/src/main/java/com/example/demo/controller/TeacherController.java
+++ jenkins-git-maven-docker-build/src/main/java/com/example/demo/controller/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,17 @@ public class TeacherController {
 
 	@GetMapping(path="/teachers")
 	public String getTeacher() {
-	List<String> list = Arrays.asList("amar","swetha","sumi");
+	List<String> list = Arrays.asList("amar","reka","sumi");
 	return list.toString();
 	}
-
+	@GetMapping(path="/teachers/{dept}")
+	public String getTeacherByDept(@PathVariable("dept") String dept) {
+	List<String> list = Arrays.asList("amar","reka","sumi");
+	
+	if(dept.equals("cse")) {
+		list = Arrays.asList("amar","swetha","sumi");		
+	}
+	return list.toString();
+	}
 }
 
